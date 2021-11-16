7f36cefdb48e04692d3955cf37a0039defd05f26
Alex <44529116+DerDunkelheit@users.noreply.github.com>
2021-11-16 21:10:00 +0300

    Create macro utils class

---
+++ Source/TPPShooter/NonUClasses/MacroUtils.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#define NAMEOF(name) #name
+#define SCREEN_LOG(key, text) if (GEngine) GEngine->AddOnScreenDebugMessage(key,2,FColor::Orange, TEXT(text));
+#define SCREEN_LOG_STRING(key, text, fstring) if (GEngine) GEngine->AddOnScreenDebugMessage(key, 2, FColor::Orange, FString::Printf(TEXT(text), fstring))
