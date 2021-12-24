64dec977d634cbf6db933a5562d9d15d84b43195
Alex <44529116+DerDunkelheit@users.noreply.github.com>
2021-12-24 13:21:43 +0300

    EditorFunctions example actor

---
+++ Source/ExampleModule/Private/Actors/EditorFunctionsExampleActor.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/EditorFunctionsExampleActor.h"
+
+
+// Sets default values
+AEditorFunctionsExampleActor::AEditorFunctionsExampleActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(MeshComponent);
+}
+
+void AEditorFunctionsExampleActor::OnConstruction(const FTransform& Transform)
+{
+	//Blueprint's Construction Script equivalent.
+	Super::OnConstruction(Transform);
+}
+
+// Called when the game starts or when spawned
+void AEditorFunctionsExampleActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AEditorFunctionsExampleActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	//We can apply something by conditional
+	const FName PropertyName(PropertyChangedEvent.Property->GetFName());
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(AEditorFunctionsExampleActor, PostEditChangeBoolToggle))
+	{
+		int test = 6;
+		//Some logic
+	}
+	
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
---
+++ Source/ExampleModule/Public/Actors/EditorFunctionsExampleActor.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EditorFunctionsExampleActor.generated.h"
+
+UCLASS()
+class EXAMPLEMODULE_API AEditorFunctionsExampleActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AEditorFunctionsExampleActor();
+	
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+protected:
+	UPROPERTY(EditAnywhere)
+	bool PostEditChangeBoolToggle;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UStaticMeshComponent* MeshComponent;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
