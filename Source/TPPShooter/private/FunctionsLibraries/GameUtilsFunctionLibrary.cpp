a6495e0b10d59026229175ee6b42af67c54b3f6a
Alex <44529116+DerDunkelheit@users.noreply.github.com>
2021-12-30 16:06:41 +0300

    Create Game utils function library

---
+++ Source/TPPShooter/private/FunctionsLibraries/GameUtilsFunctionLibrary.cpp
@@ -0,0 +1,26 @@
+﻿#include "FunctionsLibraries/GameUtilsFunctionLibrary.h"
+
+#include "Kismet/GameplayStatics.h"
+
+AActor* UGameUtilsFunctionLibrary::GetNearestActorOfClass(const UObject* WorldContextObject,
+                                                          TSubclassOf<AActor> ActorClass, FVector Location, float Radius)
+{
+	AActor* nearestActor = nullptr;
+	float nearestDistance = Radius;
+
+	TArray<AActor*> actorsOut;
+	UGameplayStatics::GetAllActorsOfClass(WorldContextObject, ActorClass, actorsOut);
+
+	for(AActor* currentActor : actorsOut)
+	{
+		float distance = FVector::Distance(Location, currentActor->GetActorLocation());
+
+		if(distance < nearestDistance)
+		{
+			nearestActor = currentActor;
+			nearestDistance = distance;
+		}
+	}
+	
+	return nearestActor;
+}
---
+++ Source/TPPShooter/public/FunctionsLibraries/GameUtilsFunctionLibrary.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameUtilsFunctionLibrary.generated.h"
+
+UCLASS()
+class TPPSHOOTER_API UGameUtilsFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	// WorldContext meta implicitly sets worldContextObject to self, so we can omit manually assigning self reference.
+	// DeterminesOutputType meta implicitly casts return type to current subclass in the BP. So we get casted AActor as an output pin.
+	UFUNCTION(BlueprintCallable, Category="GameUtilities",  meta=(WorldContext="WorldContextObject", DeterminesOutputType="ActorClass"))
+	static AActor* GetNearestActorOfClass(const UObject* WorldContextObject, TSubclassOf<AActor> ActorClass, FVector Location, float Radius = 512.f);
+};
